2e818bdb1a66f8dce27fb1b00150999779ef31ba
juncheng.du <1390851423@qq.com>
2019-10-14 15:15:54 +0800

    增加 待复习 篇数的字段

--- handler/GetReviewList.js
+++ handler/GetReviewList.js
@@ -1,52 +1,52 @@
-const base64            = require('js-base64');
+const errCode = require('../config/errCode');
+const BaseClass = require('./baseClass.js');
 
+class GetReviewList extends BaseClass {
+  constructor() {
+    super();
+  }
 
-const errCode = require("../config/errCode");
-const BaseClass = require("./baseClass.js");
+  async run(ctx, next) {
+    try {
+      // 检查params
+      const paramsOk = this.checkParams(['page', 'page_size', 'need_page']);
+      if (!paramsOk) {
+        return next();
+      }
 
-class GetReviewList extends BaseClass{
-    constructor() {
-        super();
+      let noteArr = [];
+      let waitNum = 0;
+
+      if (this.param.need_page) {
+        const page = Number(this.param.page);
+        const pageSize = Number(this.param.page_size);
+
+        const offset = (page - 1) * pageSize;
+        const limit = pageSize;
+
+        noteArr = await this.NoteModel.getReviewList(this.uid, limit, offset);
+      } else {
+        noteArr = await this.NoteModel.getReviewList(this.uid, ['id']);
+      }
+
+      const waitArr = await this.NoteModel.getReviewNum(this.uid, ['id']);
+      if (waitArr && waitArr.length !== undefined) {
+        waitNum = waitArr.length;
+      }
+      if (!noteArr || !Array.isArray(noteArr)) {
+        throw new Error('查询noteArr失败');
+      }
+      ctx.body = {
+        success: true,
+        review_list: noteArr,
+        wait_num: waitNum,
+      };
+      return next();
+    } catch (e) {
+      this.responseFail('查询noteArr失败', errCode.UPDATE_CONTNET_FAIL);
+      return next();
     }
-    async run(ctx, next) {
-        try {
-	        // 检查params
-	        let paramsOk = this.checkParams(['page', 'page_size', 'need_page']);
-	        if (!paramsOk) {
-		        return next();
-	        }
-	        
-	        let noteArr = []
-	        
-	        if (this.param.need_page) {
-		        let page = Number(this.param.page);
-		        let pageSize = Number(this.param.page_size);
-		
-		        let offset = (page - 1) * pageSize;
-		        let limit = pageSize;
-		
-		        noteArr = await this.NoteModel.getReviewList(this.uid, limit, offset);
-	        } else {
-		        noteArr = await this.NoteModel.getReviewList(this.uid, false);
-	        }
-	        
-	        
-	        
-            if (!noteArr || !Array.isArray(noteArr)) {
-                throw new Error('查询noteArr失败')
-                return
-            }
-            ctx.body = {
-                success: true,
-                review_list:noteArr,
-            }
-            return next();
-        } catch (e) {
-            console.log(e);
-            this.responseFail('查询noteArr失败', errCode.UPDATE_CONTNET_FAIL);
-            return next();
-        }
-    }
+  }
 }
 
 
--- handler/RemoveCatalog.js
+++ handler/RemoveCatalog.js
@@ -1,53 +1,53 @@
-const base64            = require('js-base64');
+const base64 = require('js-base64');
 
 // import {errCode} from "../config/errCode";
 // import {BaseClass} from './baseClass.js'
 
-const errCode = require("../config/errCode");
-const BaseClass = require("./baseClass.js");
+const errCode = require('../config/errCode');
+const BaseClass = require('./baseClass.js');
 
-class RemoveCatalog extends BaseClass{
-    constructor() {
-        super();
+class RemoveCatalog extends BaseClass {
+  constructor() {
+    super();
+  }
+
+  async run(ctx, next) {
+    try {
+      const paramOk = this.checkParams(['catalog_id']);
+
+      if (!paramOk) {
+        return next();
+      }
+
+      if (
+        typeof this.param.catalog_id !== 'number'
+      ) {
+        throw new Error('参数格式正确');
+        return;
+      }
+
+      // 确保catalog_id 是真的存在
+      const catalogArr = await this.CatalogModel.getArrByCatalogId(this.param.catalog_id, this.uid);
+      if (!Array.isArray(catalogArr) || catalogArr.length !== 1) {
+        throw new Error('catalog_id不唯一或不存在');
+        return;
+      }
+
+      const updateRes = await this.CatalogModel.updateCatalogState(this.param.catalog_id, 0);
+      if (updateRes) {
+        ctx.body = {
+          success: true,
+          message: '删除成功',
+        };
+        return next();
+      }
+      this.responseFail('网络请求失败', 0);
+      return next();
+    } catch (e) {
+      this.responseFail(e.message || '网络请求失败', 0);
+      return next();
     }
-    async run(ctx, next) {
-        try {
-            let paramOk = this.checkParams(['catalog_id'])
-
-            if (!paramOk) {
-                return next();
-            }
-
-            if (
-                typeof this.param.catalog_id !== 'number'
-            ) {
-                throw new Error('参数格式正确')
-                return;
-            }
-
-            // 确保catalog_id 是真的存在
-            let catalogArr = await this.CatalogModel.getArrByCatalogId(this.param.catalog_id, this.uid);
-            if (!Array.isArray(catalogArr) || catalogArr.length !== 1) {
-                throw new Error('catalog_id不唯一或不存在')
-                return;
-            }
-
-            let updateRes = await this.CatalogModel.updateCatalogState(this.param.catalog_id, 0);
-            if (updateRes) {
-                ctx.body = {
-                    success:true,
-                    message: '删除成功'
-                }
-                return next();
-            } else {
-                this.responseFail('网络请求失败', 0);
-                return next();
-            }
-        } catch (e) {
-            this.responseFail( e.message || '网络请求失败', 0);
-            return next();
-        }
-    }
+  }
 }
 
 module.exports = RemoveCatalog;
