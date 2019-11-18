2a06b3beea6b1921ef8c6e34da9e3899fd61fc95
juncheng.du <1390851423@qq.com>
2019-11-18 20:20:04 +0800

    增加排序功能

--- handler/GetNoteList.js
+++ handler/GetNoteList.js
@@ -1,5 +1,18 @@
 const BaseClass = require('./baseClass.js');
 
+const ORDER_MAP = {
+  1: 'gmt_create ASC',
+  2: 'gmt_create DESC',
+  3: 'gmt_modify ASC',
+  4: 'gmt_modify DESC',
+  5: 'notify_time ASC',
+  6: 'notify_time DESC',
+  7: 'review_num ASC',
+  8: 'review_num DESC',
+  9: 'frequency ASC',
+  10: 'frequency DESC',
+};
+
 class GetNoteList extends BaseClass {
   constructor() {
     super();
@@ -19,13 +32,24 @@ class GetNoteList extends BaseClass {
         return next();
       }
 
-      const noteList = await this.NoteModel.getNoteArrOrderBy(['id', 'note_id', 'title', 'content', 'need_review', 'notify_time', 'frequency', 'review_num'], {
+      const order = this.getRequestParam('order') || 2;
+
+      const noteList = await this.NoteModel.getNoteArrOrderBy([
+        'id',
+        'note_id',
+        'title',
+        'content',
+        'need_review',
+        'notify_time',
+        'frequency',
+        'review_num',
+      ], {
         catalog_id: this.param.catalog_id,
         note_id: this.param.note_id,
         user_id: this.uid,
         state: 1,
       }, [
-        'gmt_create DESC',
+        ORDER_MAP[order],
         'id ASC',
       ]);
       if (noteList) {
